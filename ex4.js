1c407f6a3b9b36ec407b74a421d8e67d190f55f7
Paul-Oyinloye <oyinloyepma@gmail.com>
2025-10-06 10:37:31 +0100

    ex4

--- ex4.js
+++ ex4.js
@@ -14,3 +14,39 @@
 //keep code DRY (Don't repeat yourself)
 
 //if you're finished, code it into a page
+
+let SumofMultiples = (a, l) => {
+    let sum = 0;
+    for (let num of 1) {
+        for (let f of a){
+            if (num % f == 0) {
+                sum += num;
+                break;
+            }
+        }
+    }
+    return sum;
+};
+
+let  eulerstring = (inputstr)  => {
+    let parts = inputstr.split(":");
+    if  (parts.length !== 2) return "Invalid Input format!";
+
+
+let factors = parts[0].trim().split(" ").map(Number);
+let multiples = parts[1].trim().split(" ").map(Number);
+
+if (factors.some(isNaN)  || multiples.sum(isNaN))
+    return "invalid number format!";
+
+let result = SumofMultiples(factors, multiples);
+
+return `${result} : ${factors.join(" ")} : ${multiples.join(" ")}`;
+};
+
+let euler4 = () => {
+  let inputStr = document.getElementById("inputStr").value;
+  let output = eulerString(inputStr);
+  document.getElementById("result").innerText = output;
+};
+
